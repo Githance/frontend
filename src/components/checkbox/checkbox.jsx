3c727c2fbd43ba5e704d152db82d6f9ed366d8d2
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-13 21:33:34 +0300

    feat: создать базовую разметку компонента checkbox

---
+++ src/components/checkbox/checkbox.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import style from "./checkbox.module.css"
+
+function CheckBox() {
+  return (
+    <label htmlFor="checkbox">
+      <input type="checkbox" name="checkbox" id="checkbox" />
+    </label>
+  );
+}
+
+export default CheckBox;
