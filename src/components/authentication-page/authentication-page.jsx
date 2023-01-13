ae8eda22b3791b9e69989d946a625b0387946555
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-13 17:08:48 +0300

    feat: сделать поле для пароля

--- src/components/authentication-page/authentication-page.jsx
+++ src/components/authentication-page/authentication-page.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Form from "../form/form";
 import Fieldset from "../fieldset/fieldset";
 import style from "./authentication-page.module.css";
 
 function AuthenticationPage() {
+  const [showPassword, setShowPassword] = useState(true);
+
+  function togglePassword() {    
+    setShowPassword((prevValue) => !prevValue);
+  }
+
   return (
     <div className={style.container}>
       <Form>
@@ -12,6 +19,15 @@ function AuthenticationPage() {
           required="required"
           label="Электронная почта"
           name="email"
+        />
+        <Fieldset
+          type={showPassword ? "password" : "text"}
+          required="required"
+          label="Пароль"
+          name="password"
+          togglePassword={() => togglePassword()}
+          showPassword={showPassword}
+          button
         />
         <div className={style.container__text}>
           <p className={style.text}>
--- src/components/fieldset/fieldset.jsx
+++ src/components/fieldset/fieldset.jsx
@@ -1,22 +1,47 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import PropTypes from "prop-types";
 import cn from "classnames";
 import style from "./fieldset.module.css";
 
-// eslint-disable-next-line react/prop-types
-function Fieldset({ type, required, label, name, className }) {
+function Fieldset({
+  type,
+  required,
+  label,
+  name,
+  className,
+  button,
+  togglePassword,
+  showPassword,
+}) {
   return (
     <fieldset className={style.fieldset}>
       <div className={cn(style.label, style.label__container, className)}>
         <label htmlFor={name} className={style.label__text}>
-          {label} {required && <span className={style.required_tag}>*</span>}
+          {label} {required && <span className={style.tag}>*</span>}
         </label>
       </div>
-      <input
-        id={name}
-        type={type}
-        required={required}
-        className={style.input}
-      />
+      <div className={cn(style.input, style.input__container)}>
+        <input
+          id={name}
+          type={type}
+          required={required}
+          className={style.input__element}
+        />
+        {button && (
+          <button
+            className={`${style.input__button} ${
+              showPassword
+                ? style.input__button_close
+                : style.input__button_open
+            }`}
+            type="button"
+            onClick={() => togglePassword()}
+          ></button>
+        )}
+      </div>
       <span className={cn(style.text)}>Error text</span>
     </fieldset>
   );
@@ -27,6 +52,9 @@ Fieldset.propTypes = {
   required: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  button: PropTypes.bool,
+  showPassword: PropTypes.bool,
+  togglePassword: PropTypes.func,
 };
 
 export default Fieldset;
