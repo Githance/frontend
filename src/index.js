f2f418a7dde0168a8c5bad55cbc4e02c159d7869
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-20 10:42:27 +0300

    feat: сделать error boundary

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/app/app";
-import "./index.css"
+import ErrorBoundary from "./components/error-boundary/error-boundary";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
