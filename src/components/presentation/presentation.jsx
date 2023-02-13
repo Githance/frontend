7a063093a92e8bb3c6227d60fed0fb6117cdc69c
Ivan <foxriver660@gmail.com>
2023-02-13 15:02:26 +0300

    feature/add presentation comp.

---
+++ src/components/presentation/presentation.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/self-closing-comp */
+import Button from '../button/button'
+import style from './presentation.module.css'
+
+export default function Presentation() {
+  return (
+    <div className={style.container}>
+      <div className={style.wrapper}>
+      <h1 className={style.title}>Мы — сообщество единомышленников</h1>
+      <p className={style.subtitle}>Делаем вместе IT-проекты для портфолио и бизнеса.
+С&nbsp;нами ты можешь создать свой проект или&nbsp;присоединиться к существующему.</p>
+<Button>Присоединиться к сообществу</Button>
+
+      </div>
+      <div className={style.man_image}></div>
+      <div className={style.woman_image}></div>
+    </div>
+  )
+}
--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable no-unused-vars */
 import Header from "../../components/header/header";
 import style from "./main-page.module.css";
+import Presentation from "../../components/presentation/presentation";
 
 function MainPage() {
-  return <Header />;
+  return (
+    <main className={style.container}>
+  <Header />
+  <Presentation/>
+  </main>
+  );
 }
 
 export default MainPage;
