8a5588e5121b5baefc40b0c44a936bbc5d5aa23a
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-14 13:02:25 +0300

    feat: add input field control logic

--- src/components/main-input/main-input.jsx
+++ src/components/main-input/main-input.jsx
@@ -1,22 +1,58 @@
+import { useState, useRef, useCallback, useEffect } from "react";
 import style from "./main-input.module.css";
 
-function MainInput({ text, onChange, onSubmit, type }) {
+function MainInput({ text, onChange, type }) {
+  const [disabledInput, setDisabledInput] = useState(true);
+
+  const inputRef = useRef(null);
 
   const inputType = (type) => {
     return type === "primary" ? true : false;
-  };
+  }; 
+
+  const changeInput = useCallback(() => {
+    console.log("change");
+    setDisabledInput((prevValue) => !prevValue);
+  }, [disabledInput]);
+
+  const blurInput = useCallback(() => {
+    console.log("blur");
+    setDisabledInput((prevValue) => !prevValue);
+  }, [disabledInput]);
+
+  const onSubmit = useCallback((e) => e.preventDefault());
+
+  useEffect(() => {
+    if (!disabledInput) {
+      console.log("focus");
+      inputRef.current.focus();
+    }
+  }, [disabledInput]);
 
   return (
     <form className={style.form} onSubmit={onSubmit}>
       <input
+        onBlur={blurInput}
+        ref={inputRef}
+        disabled={disabledInput}
         type="text"
         value={text}
         className={`${style.input} ${
-          inputType(type) ? style.input_type_primary : style.input_type_secondary
+          inputType(type)
+            ? style.input_type_primary
+            : style.input_type_secondary
         }`}
         onChange={onChange}
       />
-      <button type="submit" className={style.button}></button>
+      {disabledInput ? (
+        <button type="button" className={style.button} onClick={changeInput}>
+          Pen
+        </button>
+      ) : (
+        <button type="submit" className={style.button}>
+          Arrow
+        </button>
+      )}
     </form>
   );
 }
