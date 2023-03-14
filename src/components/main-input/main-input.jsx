07bb859cc900fb164a954c7bbf702fd2363db0e7
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-14 11:44:28 +0300

    feat: add markup and style for main-input component

--- src/components/main-input/main-input.jsx
+++ src/components/main-input/main-input.jsx
@@ -1,10 +1,23 @@
 import style from "./main-input.module.css";
 
-function MainInput() {
+function MainInput({ text, onChange, onSubmit, type }) {
+
+  const inputType = (type) => {
+    return type === "primary" ? true : false;
+  };
+
   return (
-    <div className={style.inputContainer}>
-      <div className={style.input}></div>
-    </div>
+    <form className={style.form} onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={text}
+        className={`${style.input} ${
+          inputType(type) ? style.input_type_primary : style.input_type_secondary
+        }`}
+        onChange={onChange}
+      />
+      <button type="submit" className={style.button}></button>
+    </form>
   );
 }
 
--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,7 +1,12 @@
 import style from "./profile-page.module.css";
+import MainInput from "../../components/main-input/main-input";
 
 function ProfilePage() {
-  return <div className={style.profile}></div>;
+  return (
+    <div className={style.profile}>
+      <MainInput />
+    </div>
+  );
 }
 
 export default ProfilePage;
