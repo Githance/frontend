75d421c4f5d3f6481589fa1dda50addc2e0cb100
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-14 11:14:10 +0300

    feat: add main-input component

---
+++ src/components/main-input/main-input.jsx
@@ -0,0 +1,11 @@
+import style from "./main-input.module.css";
+
+function MainInput() {
+  return (
+    <div className={style.inputContainer}>
+      <div className={style.input}></div>
+    </div>
+  );
+}
+
+export default MainInput;
