d2338b48df3558f24845ff0d5161ab051dc4c6ce
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-15 13:32:27 +0300

    feat: add icon-check component

---
+++ src/components/icon-check/icon-check.jsx
@@ -0,0 +1,8 @@
+import cn from "classnames";
+import style from "./icon-pen.module.css";
+
+function IconCheck({ className }) {
+  return <div className={cn(style.icon, className)}></div>;
+}
+
+export default IconCheck;
