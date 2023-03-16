20555af1ff02d2c484dec455004cae5061674a07
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-16 14:56:37 +0300

    feat: add base markup for main-link component

--- src/components/main-link/main-link.jsx
+++ src/components/main-link/main-link.jsx
@@ -1,7 +1,35 @@
-import style from "./main-link.module.css"
+import { useState, useCallback } from "react";
+import style from "./main-link.module.css";
+import { AnchorIcon, CheckIcon } from "../UI";
 
-const MainLink = () => {
-  return <form className={style.form}></form>;
+const MainLink = ({ link, children, onChange, onSubmit }) => {
+  const [disabledInput, setDisabledInput] = useState(true);
+
+  const changeInput = useCallback(() => {
+    console.log("change");
+    setDisabledInput((prevValue) => !prevValue);
+  }, [disabledInput]);
+
+  return (
+    <form className={style.form} onSubmit={onSubmit}>
+      <fieldset className={style.fieldset}>
+        {disabledInput ? (
+          <a href={link}>{children}</a>
+        ) : (
+          <input type="text" onChange={onChange} />
+        )}
+        {disabledInput ? (
+          <button type="button" onClick={changeInput}>
+            <AnchorIcon size="small" />
+          </button>
+        ) : (
+          <button type="submit">
+            <CheckIcon size="small" />
+          </button>
+        )}
+      </fieldset>
+    </form>
+  );
 };
 
 export default MainLink;
