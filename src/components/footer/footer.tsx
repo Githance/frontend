b3ceeb91bc0316e9897043234808ecb7c9e0dff9
Ivan <foxriver660@gmail.com>
2023-03-16 11:48:37 +0300

    initial commit

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import style from "./footer.module.css";
+
+const Footer: FC = () => {
+  return (
+    <footer className={style.footer}>
+      <div className={style.wrapper}>
+        <Link to="/" className={style.link}>
+          Пользовательское соглашение
+        </Link>
+        <Link to="/" className={style.link}>
+          support@githance.com
+        </Link>
+        <Link to="/" className={style.link}>
+          Политика конфиденциальности
+        </Link>
+
+        <p className={style.author}>© 2023, Githance </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
