49b54dc99c13d7aea6039968996d8bc7edaff580
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-21 22:33:23 +0300

    style: add fieldset-button

---
+++ src/pages/profile-page/fieldset-button/fieldset-button.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import style from './fieldset-button.module.css';
+import Button from '~/components/button/button';
+import ArrowRightIcon from '~/components/UI/icons/arrow-right-icon';
+import Divider from '~/components/UI/divider/divider';
+
+const FieldsetButton: FC = () => {
+  return (
+    <div className={style.container}>
+      <p className={style.label}>Управление</p>
+      <div className={style.container__buttons}>
+        <Button className={style.button} type="button" isValid>
+          Изменить пароль
+          <ArrowRightIcon size="small" />
+        </Button>
+        <Divider />
+        <Button className={style.button} type="button" isValid>
+          Выйти из профиля
+          <ArrowRightIcon size="small" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default FieldsetButton;
--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -4,8 +4,7 @@ import MainInput from '~/components/main-input/main-input';
 import MainLink from '~/components/main-link/main-link';
 import MainTextarea from '~/components/main-textarea/main-textarea';
 import Divider from '~/components/UI/divider/divider';
-import Button from '~/components/button/button';
-import ArrowRightIcon from '~/components/UI/icons/arrow-right-icon';
+import FieldsetButton from './fieldset-button/fieldset-button';
 
 function ProfilePage() {
   const onSubmit = useCallback((e: FormEvent) => e.preventDefault(), []);
@@ -31,18 +30,7 @@ function ProfilePage() {
             Резюме
           </MainLink>
         </div>
-        <div>
-          <p className={style.label}>Управление</p>
-          <Button className={style.button} type="button" isValid>
-            Изменить пароль
-            <ArrowRightIcon size="small" />
-          </Button>
-          <Divider />
-          <Button className={style.button} type="button" isValid>
-            Выйти из профиля
-            <ArrowRightIcon size="small" />
-          </Button>
-        </div>
+        <FieldsetButton />
       </div>
       <div className={style.profile__about}>
         <MainTextarea about="Информация" isValid onSubmit={(e: FormEvent) => onSubmit(e)} />
