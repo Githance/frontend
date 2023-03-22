3d69558462d4629200f3a6c3af70757078af083f
Ivan <foxriver660@gmail.com>
2023-03-22 16:03:14 +0300

    fix: import btn

--- src/components/UI/form/fieldset/button-fieldset/button-fieldset.tsx
+++ src/components/UI/form/fieldset/button-fieldset/button-fieldset.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Button, InputMessage } from '~/components/UI/index';
+import { Button, InputMessage } from '../../../index';
 import style from './button-fieldset.module.css';
 import cn from 'classnames';
 
