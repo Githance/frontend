aa8ac82e1d75e6fa6c45e4c4bbae17b2ff0e58a1
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-22 14:10:05 +0300

    style: add user-page component

---
+++ src/pages/user-page/user-page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import style from './user-page.module.css';
+
+const UserPage: FC = () => {
+  return <main className={style.mainPage}></main>;
+};
+
+export default UserPage;
