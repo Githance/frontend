7b3a00e55a0c350bdfe6309a573aa94c29cbd455
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-28 12:49:55 +0300

    feat: add endpoints for users api

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -4,6 +4,13 @@ class Users {
   private usersAxios = axios.create({
     baseURL: 'https://dev.githance.com/api/v1/users',
   });
+  private userData = '/me/';
+  private selectedUser(id: number) {
+    return `/${id}/`;
+  }
+  private selectedUserProjects(id: number) {
+    return `/${id}/projects/`;
+  }
 
   private checkResponse(res: AxiosResponse) {
     return res.data;
