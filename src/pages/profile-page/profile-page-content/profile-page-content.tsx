dd8664470e3500f975dbf9b62948dc72aea0170a
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-29 13:27:19 +0300

    style: add markup and style for profile-page-content

--- src/pages/profile-page/profile-page-content/profile-page-content.tsx
+++ src/pages/profile-page/profile-page-content/profile-page-content.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import cn from 'classnames';
 import { useForm } from 'react-hook-form';
 import style from './profile-page-content.module.css';
 import PageInput from '~/components/UI/page-elements/page-input/page-input';
@@ -21,6 +22,29 @@ const ProfilePageContent: FC = () => {
       <div className={style.form__name}>
         <PageInput iconSize="medium" inputSize="large" control={control} name="name" />
         <Divider />
+      </div>
+      <div className={style.form__info}>
+        <div className={style.container}>
+          <p className={style.title}>Контакты</p>
+          <div className={cn(style.container, style.container__item)}>
+            <div className={cn(style.container, style.container__inputs)}>
+              <p className={style.label}>Электронная почта (видна только вам)</p>
+              <p className={style.email}>Email</p>
+            </div>
+            <Divider />
+            <div className={cn(style.container, style.container__inputs)}>
+              <p className={style.label}>Ник в Telegram</p>
+              <PageInput iconSize="small" inputSize="medium" control={control} name="telegram" />
+            </div>
+            <Divider />
+          </div>
+        </div>
+        <div className={style.container}>
+          <p className={style.title}>Ссылки</p>
+        </div>
+        <div className={style.container}>
+          <p className={style.title}>Управление</p>
+        </div>
       </div>
     </form>
   );
