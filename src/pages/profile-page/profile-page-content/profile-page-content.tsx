3ce913a1b2c1a7aaa8568806ef8e48aef84fcc31
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-29 13:02:13 +0300

    style: add style for profile-page-content

--- src/pages/profile-page/profile-page-content/profile-page-content.tsx
+++ src/pages/profile-page/profile-page-content/profile-page-content.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import style from './profile-page-content.module.css';
 import PageInput from '~/components/UI/page-elements/page-input/page-input';
+import Divider from '~/components/UI/divider/divider';
 
 const ProfilePageContent: FC = () => {
   const { handleSubmit, control } = useForm({
@@ -17,7 +18,10 @@ const ProfilePageContent: FC = () => {
 
   return (
     <form className={style.form}>
-      <PageInput iconSize="medium" inputSize="large" control={control} name="name" />
+      <div className={style.form__name}>
+        <PageInput iconSize="medium" inputSize="large" control={control} name="name" />
+        <Divider />
+      </div>
     </form>
   );
 };
