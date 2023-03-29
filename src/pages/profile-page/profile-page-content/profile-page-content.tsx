e623d13daf11ec06278c6c110bf38552451500e5
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-29 12:21:44 +0300

    feat: add useForm hook

--- src/pages/profile-page/profile-page-content/profile-page-content.tsx
+++ src/pages/profile-page/profile-page-content/profile-page-content.tsx
@@ -1,12 +1,24 @@
 import { FC } from 'react';
+import { useForm } from 'react-hook-form';
 import style from './profile-page-content.module.css';
 import PageInput from '~/components/UI/page-elements/page-input/page-input';
 
 const ProfilePageContent: FC = () => {
+  const { setError, handleSubmit, control, formState } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      telegram: '',
+      portfolio_url: '',
+      summary_url: '',
+      bio: '',
+    },
+  });
+
   return (
-    <div className={style.content}>
-      <PageInput iconSize="medium" inputSize="large" />
-    </div>
+    <form className={style.form}>
+      <PageInput iconSize="medium" inputSize="large" control={control} />
+    </form>
   );
 };
 
