05419a90f594c484125bd0e5d34ebf5633e5c307
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-14 10:24:09 +0300

    style: add style for divider component

--- src/components/UI/divider/divider.tsx
+++ src/components/UI/divider/divider.tsx
@@ -4,12 +4,17 @@ import style from './divider.module.css';
 
 type Props = {
   weight?: 'regular' | 'bold';
+  active?: boolean;
 };
 
-const Divider: FC<Props> = ({ weight = 'regular' }) => {
+const Divider: FC<Props> = ({ weight = 'regular', active = false }) => {
   return (
     <div
-      className={cn(style.divider, weight === 'bold' ? style.divider_weight_bold : undefined)}
+      className={cn(
+        style.divider,
+        weight === 'bold' ? style.divider_weight_bold : undefined,
+        active ? style.divider_active : undefined,
+      )}
     ></div>
   );
 };
