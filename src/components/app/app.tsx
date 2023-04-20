8247b3d4865b5b3975fb4839f4296e7abcff6276
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-20 11:25:38 +0300

    feat: add useEffect in app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../../router/router';
+import token from '~/utils/token';
 
 function App() {
+  useEffect(() => {
+    if (token.getToken('accessToken')) {
+      console.log('Токен есть');
+    } else {
+      console.log('Токена нет');
+    }
+  }, []);
+
   return <RouterProvider router={router} />;
 }
 
