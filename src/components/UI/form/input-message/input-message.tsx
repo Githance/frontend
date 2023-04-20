b6abed2dd97ce98150b80d2840a5cd17b70941cd
Ivan <foxriver660@gmail.com>
2023-04-20 19:02:04 +0300

    fix: InputMessage work

--- src/components/UI/form/input-message/input-message.tsx
+++ src/components/UI/form/input-message/input-message.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import cn from 'classnames';
 import style from './input-message.module.css';
-import { addSpaceBeforeUpperCase } from '../../../../utils/addSpace';
+
 
 type Props = {
   type: 'error' | 'warning';
@@ -10,9 +10,20 @@ type Props = {
 };
 
 const InputMessage: FC<Props> = ({ type, message, className }) => {
-  const output = addSpaceBeforeUpperCase(message.join(' '));
-
-  return <p className={cn(className, type === 'error' ? style.error : style.warning)}>{output}</p>;
+  
+  return (
+    <>
+      {typeof message === 'string' ? (
+        <p className={cn(className, type === 'error' ? style.error : style.warning)}>{message}</p>
+      ) : (
+        message.map((el: any, index: number) => (
+          <p key={index} className={cn(className, type === 'error' ? style.error : style.warning)}>
+            {el}
+          </p>
+        ))
+      )}
+    </>
+  );
 };
 
 export default InputMessage;
--- src/utils/addSpace.ts
+++
@@ -1,4 +0,0 @@
-export function addSpaceBeforeUpperCase(str: any) {
-  const result = str.replace(/([a-zа-я])([A-ZА-Я])/g, '$1 $2');
-  return result;
-}
