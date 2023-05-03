98dd37b8bfdc571946dfd9847401b10bffe5457c
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-05-03 12:45:04 +0300

    feat: add loader for app component

--- src/components/presentation/presentation.tsx
+++ src/components/presentation/presentation.tsx
@@ -8,24 +8,22 @@ import { PATH } from '~/utils/variables';
 const Presentation: FC = () => {
   const isAuth = useSelector(getIsAuth);
   return (
-    <>
-      {!isAuth && (
-        <div className={style.container}>
-          <div className={style.wrapper}>
-            <h1 className={style.title}>Мы — сообщество единомышленников</h1>
-            <p className={style.subtitle}>
-              Делаем вместе IT-проекты для портфолио и бизнеса. С&nbsp;нами ты можешь создать свой
-              проект или&nbsp;присоединиться к существующему.
-            </p>
-            <Link to={`${PATH.AUTH}/${PATH.REGISTRATION}`} className={style.button}>
-              Присоединиться к сообществу
-            </Link>
-            <div className={style.man_image}></div>
-            <div className={style.woman_image}></div>
-          </div>
-        </div>
-      )}
-    </>
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <h1 className={style.title}>Мы — сообщество единомышленников</h1>
+        <p className={style.subtitle}>
+          Делаем вместе IT-проекты для портфолио и бизнеса. С&nbsp;нами ты можешь создать свой
+          проект или&nbsp;присоединиться к существующему.
+        </p>
+        {!isAuth && (
+          <Link to={`${PATH.AUTH}/${PATH.REGISTRATION}`} className={style.button}>
+            Присоединиться к сообществу
+          </Link>
+        )}
+        <div className={style.man_image}></div>
+        <div className={style.woman_image}></div>
+      </div>
+    </div>
   );
 };
 
