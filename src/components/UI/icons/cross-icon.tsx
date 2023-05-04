b829b1c06cac2f3b7310e44dca5f5b2596465cfb
Ivan <foxriver660@gmail.com>
2023-05-04 14:51:02 +0300

    feat: add Btn in vacancy page

---
+++ src/components/UI/icons/cross-icon.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+const CrossIcon: FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" fill="none">
+      <path
+        fill="#fff"
+        d="M50.5 29.071H29.071V50.5H21.93V29.071H.5V21.93h21.429V.5h7.142v21.429H50.5v7.142Z"
+      />
+    </svg>
+  );
+};
+
+export default CrossIcon;
