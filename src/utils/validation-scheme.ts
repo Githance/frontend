fe799e54a1fdd528e87409bf059f2928cfcd1812
Ivan <foxriver660@gmail.com>
2023-05-22 12:54:05 +0300

    refactor: validation in modal create project

--- src/utils/validation-scheme.ts
+++ src/utils/validation-scheme.ts
@@ -3,3 +3,8 @@ const minLength = { value: 8, message: 'Минимум 8 символов' };
 
 export const RequireValidationScheme = { required: 'Заполни меня' };
 export const PassValidationScheme = { required: 'Заполни меня', minLength: minLength };
+export const TextareaValidationScheme = {
+  required: 'Заполни меня',
+  minLength: { value: 3, message: 'Минимум 3 символа' },
+  maxLength: { value: 300, message: 'Максимум 300 символов' },
+};
