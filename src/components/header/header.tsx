2e6e609efe63694b65ce044dfe87fe5e6ec3f8a4
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-02 10:24:12 +0300

    refactor: change header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
 import cn from 'classnames';
+import { useSelector } from '~/services/hooks';
 import { NavLink, useMatch } from 'react-router-dom';
-import { Logo, NotificationIcon, ProfileIcon } from '../UI';
+import { Logo, ProfileIcon } from '../UI';
 import style from './header.module.css';
 import { PATH } from '~/utils/variables';
+import { getIsAuth } from '~/services/selectors';
 
 const Header: FC = () => {
   const profile = useMatch('profile');
+  const isAuth = useSelector(getIsAuth);
 
   return (
     <header className={style.header}>
@@ -15,25 +18,38 @@ const Header: FC = () => {
           <NavLink className={`${style.link} pr-7`} to="/">
             <Logo />
           </NavLink>
-          <NavLink className={style.link} to="/">
-            Мои проекты
-          </NavLink>
-          <NavLink className={style.link} to="/">
-            Мои заявки
-          </NavLink>
+          {isAuth && (
+            <>
+              <NavLink className={style.link} to="/">
+                Мои проекты
+              </NavLink>
+              <NavLink className={style.link} to="/">
+                Мои заявки
+              </NavLink>
+            </>
+          )}
         </div>
         <div className={style.nav__profile}>
-          <NavLink className={cn(style.link, style.link_position_profile)} to="/">
+          {/* Временно закоментированна ссылка, функционал будем реализовывать сразу после
+          MVP */}
+          {/* <NavLink className={cn(style.link, style.link_position_profile)} to="/">
             <NotificationIcon type="default" />
             Уведомления
-          </NavLink>
-          <NavLink
-            className={cn(style.link, style.link_position_profile, profile && style.link_active)}
-            to={PATH.PROFILE}
-          >
-            <ProfileIcon active={!!profile} />
-            Профиль
-          </NavLink>
+          </NavLink> */}
+          {isAuth ? (
+            <NavLink
+              className={cn(style.link, style.link_position_profile, profile && style.link_active)}
+              to={PATH.PROFILE}
+            >
+              <ProfileIcon active={!!profile} />
+              Профиль
+            </NavLink>
+          ) : (
+            <NavLink className={cn(style.link, style.link_position_profile)} to={PATH.AUTH}>
+              <ProfileIcon />
+              Вход
+            </NavLink>
+          )}
         </div>
       </nav>
     </header>
